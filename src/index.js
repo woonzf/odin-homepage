12e70b336259e40f6a0e1702758f07186556fec1
woonzf <137765412+woonzf@users.noreply.github.com>
2024-05-16 22:45:30 +0800

    Add animations and test functions to nav buttons

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import "./style.css";
 import { float } from "./js/float";
 import { darkMode } from "./js/dark-mode";
+import { menu } from "./js/menu";
 
 // float.init();
 darkMode.init();
+menu.init();
--- src/js/menu.js
+++ src/js/menu.js
@@ -1,10 +1,72 @@
 const menu = (() => {
-    const btnMenu = document.querySelector();
-    function init() {
+  const btnAboutL = document.querySelector("#btn-about-l");
+  const btnProjectsL = document.querySelector("#btn-projects-l");
+  const btnContactL = document.querySelector("#btn-contact-l");
 
-    }
+  const btnMenu = document.querySelector("#btn-menu");
+  const btnMenuBack = document.querySelector("#btn-menu-back");
+  const btnAboutP = document.querySelector("#btn-about-p");
+  const btnProjectsP = document.querySelector("#btn-projects-p");
+  const btnContactP = document.querySelector("#btn-contact-p");
 
-    return { init }
-})()
+  const hellos = document.querySelectorAll(".hello");
+  const helloDot = document.querySelector("#hello-dot");
+  const menu = document.querySelector("#menu");
 
-export { menu }
+  function init() {
+    btnAboutL.onclick = () => {
+      console.log("ABOUT");
+    };
+
+    btnProjectsL.onclick = () => {
+      console.log("PROJECTS");
+    };
+
+    btnContactL.onclick = () => {
+      console.log("CONTACT");
+    };
+
+    btnMenu.onclick = () => {
+      _openMenuTimeout(btnMenu, menu);
+    };
+
+    btnMenuBack.onclick = () => {
+      _openMenuTimeout(menu, btnMenu);
+    };
+
+    btnAboutP.onclick = () => {
+      console.log("ABOUT P");
+    };
+
+    btnProjectsP.onclick = () => {
+      console.log("PROJECTS P");
+    };
+
+    btnContactP.onclick = () => {
+      console.log("CONTACT P");
+    };
+  }
+
+  function _openMenuTimeout(hide, show) {
+    setTimeout(() => {
+      hide.classList.toggle("hidden");
+    }, 1000);
+    show.classList.toggle("hidden");
+    _openMenu();
+  }
+
+  function _openMenu() {
+    requestAnimationFrame(() => {
+      menu.classList.toggle("open");
+      btnMenu.classList.toggle("open");
+    });
+    hellos.forEach((el) => {
+      el.classList.toggle("move");
+    });
+    helloDot.classList.toggle("move");
+  }
+
+  return { init };
+})();
+
+export { menu };
