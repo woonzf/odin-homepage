e72c33db9bd6951964efc15d8c8bddc78989b52a
woonzf <137765412+woonzf@users.noreply.github.com>
2024-06-11 21:33:51 +0800

    Remake nav layout in landscape mode

--- src/js/menu.js
+++ src/js/menu.js
@@ -3,21 +3,30 @@ import { scroll } from "./scroll";
 import { tabClickable } from "./tab-clickable";
 
 const menu = (() => {
+  const btnMenuL = document.querySelector("#btn-menu-l");
+  const btnMenuBackL = document.querySelector("#btn-menu-back-l");
+
   const btnAboutL = document.querySelector("#btn-about-l");
   const btnProjectsL = document.querySelector("#btn-projects-l");
   const btnContactL = document.querySelector("#btn-contact-l");
 
-  const btnMenu = document.querySelector("#btn-menu");
-  const btnMenuBack = document.querySelector("#btn-menu-back");
+  const btnMenuP = document.querySelector("#btn-menu-p");
+  const btnMenuBackP = document.querySelector("#btn-menu-back-p");
 
   const btnHomeP = document.querySelector("#btn-home-p");
   const btnAboutP = document.querySelector("#btn-about-p");
   const btnProjectsP = document.querySelector("#btn-projects-p");
   const btnContactP = document.querySelector("#btn-contact-p");
 
-  const menu = document.querySelector("#menu");
+  const menuL = document.querySelector("#menu-l");
+  const menuP = document.querySelector("#menu-p");
   const helloWrapper = document.querySelector("#hello-wrapper");
   const infoWrapper = document.querySelector("#info-wrapper");
+
+  const bgLeftMenu = document.querySelector("#bg-left-menu");
+  const bgLeftAbout = document.querySelector("#bg-left-about");
+  const bgLeftProjects = document.querySelector("#bg-left-projects");
+  const bgLeftContact = document.querySelector("#bg-left-contact");
 
   const about = document.querySelector("#about");
   const projects = document.querySelector("#projects");
@@ -32,14 +41,38 @@ const menu = (() => {
   function init() {
     threshold = scroll.getScrollThreshold();
 
+    btnMenuL.onclick = () => {
+      btnMenuL.classList.toggle("hide");
+      menuL.classList.toggle("hidden");
+      requestAnimationFrame(() => {
+        menuL.classList.toggle("open");
+      });
+      helloWrapper.classList.toggle("hide");
+      infoWrapper.classList.toggle("hide");
+      bgLeftMenu.classList.toggle("show");
+      isOpenMenu = 1;
+    };
+
+    btnMenuBackL.onclick = () => {
+      helloWrapper.classList.toggle("hide");
+      infoWrapper.classList.toggle("hide");
+      bgLeftMenu.classList.toggle("show");
+      menuL.classList.toggle("open");
+      setTimeout(() => {
+        menuL.classList.toggle("hidden");
+      }, 1000);
+      btnMenuL.classList.toggle("hide");
+      isOpenMenu = 0;
+    };
+
     btnAboutL.onclick = () => {
-      _openTab(about, 2, btnAboutL);
+      _openTab(about, 1, btnAboutL);
     };
     btnProjectsL.onclick = () => {
       _openTab(projects, 1, btnProjectsL);
     };
     btnContactL.onclick = () => {
-      _openTab(contact, 2, btnContactL);
+      _openTab(contact, 1, btnContactL);
     };
 
     btnBacks.forEach((btn) => {
@@ -48,9 +81,9 @@ const menu = (() => {
       };
     });
 
-    btnMenu.onclick = () => {
+    btnMenuP.onclick = () => {
       if (isOpenMenu === 1) {
-        btnMenuBack.click();
+        btnMenuBackP.click();
         return;
       }
 
@@ -59,21 +92,21 @@ const menu = (() => {
 
       if (window.scrollY < threshold) _toggleBackground(1);
 
-      menu.classList.toggle("hidden");
+      menuP.classList.toggle("hidden");
       requestAnimationFrame(() => {
-        menu.classList.toggle("open");
+        menuP.classList.toggle("open");
       });
 
       isOpenMenu = 1;
     };
 
-    btnMenuBack.onclick = () => {
+    btnMenuBackP.onclick = () => {
       _toggleMenuCooldown();
       scroll.enableScroll();
-      menu.classList.toggle("open");
+      menuP.classList.toggle("open");
 
       setTimeout(() => {
-        menu.classList.toggle("hidden");
+        menuP.classList.toggle("hidden");
       }, 500);
 
       if (window.scrollY < threshold) _toggleBackground(1);
@@ -138,11 +171,11 @@ const menu = (() => {
   }
 
   function _toggleMenuCooldown() {
-    btnMenu.disabled = true;
-    btnMenuBack.disabled = true;
+    btnMenuP.disabled = true;
+    btnMenuBackP.disabled = true;
     setTimeout(() => {
-      btnMenu.disabled = false;
-      btnMenuBack.disabled = false;
+      btnMenuP.disabled = false;
+      btnMenuBackP.disabled = false;
     }, 500);
   }
 
@@ -152,7 +185,7 @@ const menu = (() => {
       content.scrollIntoView();
       if (offset === 1) window.scrollBy(0, -56 - 28);
     }
-    btnMenuBack.click();
+    btnMenuBackP.click();
   }
 
   return { init, resetOnScreenChange };
